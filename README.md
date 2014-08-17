03f73c87aa47ea6a0bc9534d66084772ab10c0b2
bealox <thebealox@gmail.com>
2014-08-17 21:11:13 +1000

    reinitialize files

---
+++ README.md
@@ -0,0 +1,1 @@
+Store different version of vimrc to keep track of change in progress.
---
+++ vimrc-1/install.sh
@@ -0,0 +1,4 @@
+CURRENT_DIR=$(pwd)
+echo installing vimrc from $CURRENT_DIR to $HOME
+ln -s $CURRENT_DIR/vimrc $HOME/.vimrc
+echo finished..
