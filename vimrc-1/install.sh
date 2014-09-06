e45f71c8f198b944f1978eaaa9ff46036d0dc516
bealox <thebealox@gmail.com>
2014-09-07 08:49:37 +1000

    chnage install bash script in vimrc-1

--- vimrc-1/install.sh
+++ vimrc-1/install.sh
@@ -1,4 +1,21 @@
 CURRENT_DIR=$(pwd)
+
+die() {
+	echo "bye bye"
+	exit 1
+}
+
+checkFileExist() {
+	if [ -f $HOME/.vimrc ]; then
+		read -p "Overriding the existing vimrc file?[yn]" answer
+		if [[ $answer = y ]]; then
+			ln -fs $CURRENT_DIR/vimrc $HOME/.vimrc
+			echo finished..
+			exit 0
+		else
+			die
+		fi
+	fi
+}
 echo installing vimrc from $CURRENT_DIR to $HOME
-ln -s $CURRENT_DIR/vimrc $HOME/.vimrc
-echo finished..
+checkFileExist
